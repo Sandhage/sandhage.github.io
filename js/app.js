fd5709c019b5a98ce71994e59299bb1dc5ebaad7
Sandhage <kylesandhage@gmail.com>
2017-06-22 00:46:55 -0400

    Updated project pages: removed kaplow quiz, added relay network, reordered nav. Added keyboard svg asset.

--- js/app.js
+++ js/app.js
@@ -29,21 +29,12 @@ function topNavClick() {
 			meadow2   = "A Calming Meadow, on the surface, is a very simple website. It shows images of fields and blades of tall grass blowing in the wind with only a prompt for your zip code. When you give this website your zipcode, it is then able to assign latitude and longitude coordinates to an api query to find out what time the sun sets in your zipcode. If the sun is still out, nothing changes. Come sundown, however, the app changes to a more menacing view and calls a current story from www.reddit.com/r/nosleep at random.",
 			list1     = "Shopping List is a fairly straightforward project: I made this app as a way to give myself an introduction into Ruby on Rails. Understanding the MVC, routing, resources, migrations... it through me for a loop at first; by the end I had the swing of it, though, and had an app that creates dynamic pages, lists, and dependent child items.",
 			list2     = "The hardest part of this project for me was learning about routing and making sure all of the pathing was correct. There were a number of errors that spanned the gamut from segments of code that were barking up the wrong tree to frustratingly minor spelling errors. It was a good experience and I learned quite a bit.",
-			calendar1 = "This is my current project! I'm working on a calendar app for the Orbiter 3 theater company in Philadelphia. Their focus is on producing plays written by new local playwrights and I am super excited to help them with this app. I will have a few more details here when the project is finished!"
-			calendar2 = " "
-			// calendar1 = "Orbiter 3 is a theater production company in Philadelphia; one of the key members is a friend of mine who knew I love web development and they were looking to create a calendar app for their fundraising project -- a 365 day project where they try to get a sponser for each day to donate $3.65, $36.50, or $365.00. They wanted the ability for users to log in and claim a specific day to donate.",
-			// calendar2 = "One of the big challenges was that they used Squarespace to create their website. The design was great, but as a service Squarespace doesn't play well with 3rd party features. To get around this problem, I cloned the design as best I could on my own and they were able to place the app under a subdomain (calendar.orbiter3.com), while hosting it through Heroku.";
+			calendar1 = "This was my first freelance project -- I built a calendar app for the Orbiter 3 theater company in Philadelphia. Their focus is on producing plays written by new local playwrights and I was super excited to help them with this app."
+			calendar2 = "The calendar was built using Ruby on Rails. It was fun to work on; I implemented the OAuth gem in order to allow for twitter and facebook login/authentication."
+			relay1 = "Relay Network is a communications/technology startup located in Radnor, PA. I work there full-time as a front-end engineer. My job is focused on maintaining and developing new features for two of our applications: the client portal and our messaging platform."
+			relay2 = "Our client facing applications were originally built using the Backbone.js framework on the front-end with a back-end written almost strictly in clojure. However, we recently started to rewrite these apps in Angular 2, which I'm naturally hyped about.";
 
-		if ( $(this).is('#nav-kaplow') ) {
-			$('#project-title').html("Kaplow Quiz");
-			$('#description1').html(kaplow1);
-			$('#description2').html(kaplow2);
-			$('#project-link').html("link to project");
-			$('#project-link').prop("href", "http://sandhage.github.io/kaplow-quiz/");
-			$('#project-img-container').css("padding-top", "50px");
-			$('#project-img').prop("src", "assets/icon-bomb.svg");
-			$('#project-detail-container').show();
-		} else if ( $(this).is('#nav-meadow') ) {
+		if ( $(this).is('#nav-meadow') ) {
 			$('#project-title').html("A Calming Meadow");
 			$('#description1').html(meadow1);
 			$('#description2').html(meadow2);
@@ -66,10 +57,18 @@ function topNavClick() {
 			$('#description1').html(calendar1);
 			$('#description2').html(calendar2);
 			$('#project-link').html("link to project");
-			// $('#project-link').prop("href", "http://sandhage.github.io/orbiter-3-calendar");
-			$('#project-link').prop("href", "#");
+			$('#project-link').prop("href", "http://orbiter-3-calendar-production.herokuapp.com/");
 			$('#project-img-container').css("padding-top", "30px");
 			$('#project-img').prop("src", "assets/icon-calendar.svg")
+			$('#project-detail-container').show();
+		} else if ( $(this).is('#nav-relay') ) {
+			$('#project-title').html("Relay Network");
+			$('#description1').html(relay1);
+			$('#description2').html(relay2);
+			$('#project-link').html("link to website");
+			$('#project-link').prop("href", "http://relaynetwork.com/");
+			$('#project-img-container').css("padding-top", "30px");
+			$('#project-img').prop("src", "assets/keyboard.svg")
 			$('#project-detail-container').show();
 		} else if ( $(this).is('#nav-about') ) {
 			$('#about-me-container').show();
